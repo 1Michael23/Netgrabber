32d24a2fc18050f2f542e86a8ca3eedadd06754d
jnawotka <38390397+jnawotka@users.noreply.github.com>
2020-08-18 11:49:53 +1000

    Change the way the TXT file is layed out
    It is a bit more intuitive

--- netgrabber.py
+++ netgrabber.py
@@ -1,9 +1,11 @@
-#    _   _      _    ____           _     _
-#   | \ | | ___| |_ / ___|_ __ __ _| |__ | |__   ___ _ __
-#   |  \| |/ _ \ __| |  _| '__/ _` | '_ \| '_ \ / _ \ '__|
-#   | |\  |  __/ |_| |_| | | | (_| | |_) | |_) |  __/ |
-#   |_| \_|\___|\__|\____|_|  \__,_|_.__/|_.__/ \___|_|
+logo = r"""
+ _   _      _    ____           _     _
+| \ | | ___| |_ / ___|_ __ __ _| |__ | |__   ___ _ __
+|  \| |/ _ \ __| |  _| '__/ _` | '_ \| '_ \ / _ \ '__|
+| |\  |  __/ |_| |_| | | | (_| | |_) | |_) |  __/ |
+|_| \_|\___|\__|\____|_|  \__,_|_.__/|_.__/ \___|_|
 
+"""
 #Grabs the mac from all attached network cards
 
 import socket,psutil,os
@@ -17,9 +19,9 @@ addrs = psutil.net_if_addrs()
 
 rawnics = (ni.interfaces())
 
-f = open(hostname, 'w+')
+f = open(hostname+'.txt', 'w+')
 
-f.write("Hostname: " + str(hostname) + '\n\n')
+f.write(logo+"Netgrabber Results For Host: " + str(hostname) + '\n\n')
 
 #grab ip and mac from all adapters
 
@@ -28,13 +30,15 @@ for i in rawnics:
         ip = ni.ifaddresses(i)[2][0]['addr']
 
     except KeyError:
-        ip = "Skipped (No IP Found)"
+        ip = "No IP Found"
     if os.name == 'nt':
         mac_address = ni.ifaddresses(i)[-1000][0]['addr']
     elif os.name == 'posix':
         mac_address = gma(interface=i)
+    if str(mac_address) == "00:00:00:00:00:00":
+        mac_address = "No Mac Address Found"
     #writes the hostname, ip(s) and mac address(s) to the text file
-    f.write(i + ': Ip: '+str(ip)+'\n' + i + ": Mac address: " + str(mac_address) + '\n\n')
+    f.write('Adaptor Name: '+i + '\n     Ip: '+str(ip)+ "\n     Mac address: " + str(mac_address) + '\n\n')
 
 #saves data and releases text file
 
